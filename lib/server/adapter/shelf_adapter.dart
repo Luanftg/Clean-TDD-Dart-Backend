117d6326cee6478bb05cb612d496dd7673e07857
Luanftg <luanftgimenez@gmail.com>
2023-07-03 08:29:26 -0300

    [feat]: add status and REquestParams to Adapter

--- lib/server/adapter/shelf_adapter.dart
+++ lib/server/adapter/shelf_adapter.dart
@@ -2,9 +2,8 @@ part of '../server.dart';
 
 class ShelfAdapter {
   List<Controller> controllers;
-  ShelfAdapter({
-    required this.controllers,
-  });
+
+  ShelfAdapter({required this.controllers});
 
   void handler(Router router) {
     for (final controller in controllers) {
@@ -20,10 +19,17 @@ class ShelfAdapter {
       final String verb = entry.key;
 
       router.add(verb, route, (Request request) async {
-        final responseHandler = await entry.value(RequestParams());
+        final payload = await request.readAsString();
+        final responseHandler = await entry.value(RequestParams(
+          body: payload.isEmpty ? null : jsonDecode(payload),
+        ));
         switch (responseHandler.status) {
           case StatusHandler.ok:
             return ResponseJSON.ok(responseHandler.body);
+          case StatusHandler.created:
+            return ResponseJSON.created(responseHandler.body);
+          case StatusHandler.badRequest:
+            return ResponseJSON.badRequest(responseHandler.body);
           default:
             return Response.internalServerError();
         }
